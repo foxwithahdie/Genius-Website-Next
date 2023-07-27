69db8efce08eae82139ca777c662d7d68fc74c3d
Avery R <contact@nekodjin.org>
2023-07-26 22:14:45 -0400

    mobile layout (WIP)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "@fontsource-variable/inter";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,7 +14,34 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex min-h-screen w-full flex-col supports-dvh:min-h-dscreen">
+          <header className="px-4 py-2 shadow shadow-neutral-300 drop-shadow">
+            <nav className="flex w-full items-center justify-between">
+              <a href="/" className="font-futura text-sm font-medium">
+                GENIUS WITHIN
+              </a>
+              <button className="rounded border-[1px] border-neutral-200 p-2">
+                {/* TODO: replace this with a better hamburger icon (custom?) */}
+                {/* TODO: make the hamburger menu functional */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M3 18v-2h18v2H3Zm0-5v-2h18v2H3Zm0-5V6h18v2H3Z"
+                  />
+                </svg>
+              </button>
+            </nav>
+          </header>
+          <div className="flex grow flex-col bg-neutral-100">{children}</div>
+          {/* TODO: do we need a footer? it's not in the figma */}
+          <footer></footer>
+        </div>
+      </body>
     </html>
   );
 }
